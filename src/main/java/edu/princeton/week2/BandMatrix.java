97d642e01a418cc58626c9d5fc5e27007cfdcddc
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-28 17:22:00 -0400

    Initial commit

--- src/main/java/edu/princeton/week2/BandMatrix.java
+++ src/main/java/edu/princeton/week2/BandMatrix.java
@@ -1,27 +1,30 @@
 package edu.princeton.week2;
 /**
  Band matrices. Write a program BandMatrix.
+
+ - BAND MATRIX is a matrix that have all elements 0 except a band on the main diagonal.
+ - When you have a band of Astrix below and above it is called Tri-Diagonal matrix because if you are to measure the width of the diagonal it is going to be 3 tall
+
  java that takes two integer command-line arguments n and width
  and prints an n-by-n pattern like the ones below,
  with a zero (0) for each element whose distance from the main diagonal is strictly more than width,
- and an asterisk (*) for each entry that is not,
- and two spaces between each 0 or *.Here, distance means the minimum number of cells you have to move
- (either left, right, up, or down) to reach any element on the main diagonal.
+ and an asterisk (*) for each entry that is not,and two spaces between each 0 or *.
+ (distance means the minimum number of cells you have to move
+ (either left, right, up, or down) to reach any element on the main diagonal).
  */
 
 public class BandMatrix {
     public void bandMatrix(int n , int width){
-        int i;
-        int j;
+
         // forms nxn patterns
 
-        for(i=0; i<n; i++){
-            for(j=0;j<n;j++){
-                if(j==i || Math.abs(i-j) <= width){ //the j's distance is compared with i entry(main diagonal) less than or equal width and prints asterisks *
-                    System.out.printf("*" + " ");// true condition displays "*"
+        for(int i=0; i<n; i++){ // row index
+            for(int j=0;j<n;j++){ // col index
+                if(j==i || Math.abs(i-j) <= width){ // calculates the distance of each element from its main diagonal
+                    System.out.print("*" + " ");// if the distance between i and j is equal or the diff is less than or equal to width
                 }
                 else{
-                    System.out.printf("0" + " ");// false displays "0"
+                    System.out.printf("0" + " ");// for each element whose distance from the main diagonal is more than width,
                 }
 
             }
