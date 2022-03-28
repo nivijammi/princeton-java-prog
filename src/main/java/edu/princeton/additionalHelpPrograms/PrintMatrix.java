900bb61d78ef65acba6922fa083ee95c01d7a704
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-28 16:21:22 -0400

    Initial commit

--- src/main/java/edu/princeton/additionalHelpPrograms/PrintMatrix.java
+++ src/main/java/edu/princeton/additionalHelpPrograms/PrintMatrix.java
@@ -1,2 +1,21 @@
-package edu.princeton.additionalHelpPrograms;public class PrintMatrix {
+package edu.princeton.additionalHelpPrograms;
+
+public class PrintMatrix {
+    public void printMatrix(int n){
+        int num =0;
+        for(int row =0;row<n;row++){
+            for(int col = 0; col<n;col++){
+                System.out.print(num+ " ");
+                num++;
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        PrintMatrix object = new PrintMatrix();
+        object.printMatrix(3);
+    }
 }
