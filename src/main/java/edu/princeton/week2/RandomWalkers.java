9b98bdf3fc214fe6e4da874b8f4b75c7c3ece894
Nivi Jammi <nivedita.jammi@gmail.com>
2022-03-31 12:02:31 -0400

    Initial commit

---
+++ src/main/java/edu/princeton/week2/RandomWalkers.java
@@ -0,0 +1,20 @@
+package edu.princeton.week2;
+/**
+ * Random walkers. Write a program RandomWalkers.
+ * java that takes two integer command-line arguments r and trials.
+ * In each of trials independent experiments, simulate a random walk
+ * until the random walker is at Manhattan distance r from the starting point.
+ * Print the average number of steps.
+ *
+ * As r increases, we expect the random walker to take more and more steps.
+ * But how many more steps?
+ * Use RandomWalkers.java to formulate a hypothesis
+ * as to how the average number of steps grows as a function of r.
+ *
+ * Estimating an unknown quantity by generating random samples and aggregating the results
+ * is an example of Monte Carlo simulation—a powerful computational technique
+ * that is used widely in statistical physics, computational finance, and computer graphics.
+ */
+
+public class RandomWalkers {
+}
