84e21ecb65c5b2a3b2ae53713d4e430677e91098
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-04 17:16:31 -0400

    Initial commit

--- src/main/java/edu/princeton/lectureCodeWeek3/CreateADeckOfCards.java
+++ src/main/java/edu/princeton/lectureCodeWeek3/CreateADeckOfCards.java
@@ -3,7 +3,7 @@ package edu.princeton.lectureCodeWeek3;
 
 import java.sql.SQLOutput;
 
-public class DeckOfCards {
+public class CreateADeckOfCards {
     public static void main(String[] args) {
         String[] rank = {"2","3","4","5","6","7","8","9","10","J","Q","K","A"};
         String[] suit = {"club", "diamond", "heart", "spade"};
