157e2ae47f4020c30b89c9e7ebfeb78fbc2a49e9
Nivi Jammi <nivedita.jammi@gmail.com>
2022-04-09 01:10:26 -0400

    Initial commit

--- src/test/java/edu/princeton/week3/DiscreteDistributionTest.java
+++ src/test/java/edu/princeton/week3/DiscreteDistributionTest.java
@@ -1,4 +1,16 @@
+package edu.princeton.week3;
+
 import junit.framework.TestCase;
-public class DiscreteDistributionTest extends TestCase {
-  
+import org.junit.Test;
+
+public class DiscreteDistributionTest  {
+    @Test
+    public void shouldReturnMRandomIndicesChoosingEachIndexIWithProbabilityProportionalToAi(){
+        DiscreteDistribution object = new DiscreteDistribution();
+        int[] n = {10,10,10,10,10,10,50};
+        object.uniqueIndex(n);
+
+    }
+ {
+    }
 }
